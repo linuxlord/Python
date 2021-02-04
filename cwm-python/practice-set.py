926270ee4c8820b283d7f6fe7e6eda4ebf14b4df
linuxlord <abhishekce@ymail.com>
2021-02-04 23:00:49 +0530

    Initiating python

---
+++ cwm-python/practice-set.py
@@ -0,0 +1,10 @@
+import keyword
+print('''I'm a great programmer''')
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+print(help(keyword))
+print('*' * 10)
+print("Hello WOrld")
+x = 1
+unit_price = 3
+y = 4
